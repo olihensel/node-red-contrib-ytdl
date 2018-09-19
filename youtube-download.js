db71dca3fda288b7077a162b9af75532df23781e
Luca Mazzilli <setmaat@gmail.com>
2018-09-19 22:21:29 +0200

    filename from video title

--- youtube-download.js
+++ youtube-download.js
@@ -35,28 +35,45 @@ module.exports = function(RED){
               return;
             }
 
+            if(!path_.endsWith("/")){
+               path_+="/";
+            }
+
             progressStream.on('progress', function(progress) {
                 node.status({fill:"blue", shape:"ring", text:progress.percentage.toFixed(2) + "%"});
             });
 
             //default video
             var options = { filter: function(format) { return format.container === 'mp4'; } };
+            var ext = "mp4";
             //audio only
             if(msg.audioonly){
               options =  { filter: 'audioonly'};
+              ext = "mp3";
             }
 
-            var ytdl = Ytdl(url_, options);
+            var download = function(url_, path_, options){
+              var ytdl = Ytdl(url_, options);
 
-            ytdl.on('response', function(response) {
-                progressStream.setLength( response.headers["content-length"] );
+              ytdl.on('response', function(response) {
+                  progressStream.setLength( response.headers["content-length"] );
+              });
+              ytdl.pipe(progressStream).pipe(Fs.createWriteStream(path_))
+              .on('finish', function(){
+                  node.status({fill:"blue", shape:"dot", text:"Done"});
+                  node.send(msg);
+              })
+              node.ytdl = ytdl;
+            }
+
+            //get info
+            Ytdl.getInfo(url_, function(err, info) {
+              //video title as file name
+              msg.file = path_+info.title+"."+ext;
+              download(url_, msg.file , options);
             });
-            ytdl.pipe(progressStream).pipe(Fs.createWriteStream(path_))
-            .on('finish', function(){
-                node.status({fill:"blue", shape:"dot", text:"Done"});
-                node.send(msg);
-            })
-            node.ytdl = ytdl;
+
+
         });
 
         this.on('close', function(){
